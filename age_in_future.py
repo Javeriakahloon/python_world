926bee346a676e342b0921fe4e9bbf0a6c2c7bfd
Javeria Ameer <javeria22816@gmail.com>
2025-06-01 02:38:13 -0700

    Create age_in_future.py

---
+++ age_in_future.py
@@ -0,0 +1,8 @@
+current_age = int(input('Enter your current age: '))
+current_year = int(input('Enter the current year: '))
+years_in_future = int(input('Enter number of years in the future: '))
+
+future_year = current_year + years_in_future
+future_age = current_age + years_in_future
+
+print(f'In {future_year}, you will be {future_age} years old.')
